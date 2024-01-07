28778d492328839c1c3bfba419b3ca2d5982b1f9
Alexandra Schukina <alexandra.kovalenko1999@mail.ru>
2024-01-07 14:43:52 +0300

    rerite test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,4 +19,17 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxFirstNotMax () {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {7, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        // поменяла первое значение на меньшее число, так как первое значение в массиве подходило под условие currentMax
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
